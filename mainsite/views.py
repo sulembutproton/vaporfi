eebb839cad9c51a2aca9a6fbc3b509b03805a26e
Vaporfi <sulembut@protonmail.com>
2021-07-04 01:32:37 +0000

    Welcome to Vaporfi

---
+++ mainsite/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def google345fc5d6c0589fc4(request):
+    return render(request, 'google345fc5d6c0589fc4.html', {})
+
+def vaporfi(request):
+    return render(request, 'vaporfi.html', {})
+
+
